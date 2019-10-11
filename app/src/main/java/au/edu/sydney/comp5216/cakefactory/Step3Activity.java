4ebd9e5bd52e5e5ff74b3657740adcbe779f6c5f
Vivihhw <huhu6404@uni.sydney.edu.au>
2019-10-11 14:24:54 +1100

    Add select cake flavour view

--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/Step3Activity.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/Step3Activity.java
@@ -1,26 +1,19 @@
 package au.edu.sydney.comp5216.cakefactory;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Step2Activity extends AppCompatActivity {
+public class Step3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.step2);
+        setContentView(R.layout.step3);
         this.overridePendingTransition(R.anim.anim_slide_in_left,
                 R.anim.anim_slide_out_left);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
-    }
-    public void goNext(View view) {
-        Intent intent = new Intent(Step2Activity.this, Step3Activity.class);
-        startActivity(intent);
 
     }
 }
