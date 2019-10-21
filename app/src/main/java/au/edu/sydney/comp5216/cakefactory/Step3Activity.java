ba3726a41241b4d1dafda07cbabdf18564b6cb52
Vivihhw <huhu6404@uni.sydney.edu.au>
2019-10-21 15:36:11 +1100

    Add Step 3 select boarder

--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/Step3Activity.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/Step3Activity.java
@@ -1,14 +1,22 @@
 package au.edu.sydney.comp5216.cakefactory;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Step3Activity extends AppCompatActivity {
+    ImageView chocolate;
+    ImageView cheese;
+    ImageView matcha;
+    ImageView strawberry;
+    ImageView vanilla;
+    String selected;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.step3);
@@ -16,6 +24,7 @@ public class Step3Activity extends AppCompatActivity {
                 R.anim.anim_slide_out_left);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        drawBorder();
 
     }
     public void goNext(View view) {
@@ -26,6 +35,76 @@ public class Step3Activity extends AppCompatActivity {
     public void goBack(View view) {
         Intent intent = new Intent(Step3Activity.this, Step2Activity.class);
         startActivity(intent);
+
+    }
+
+    public void drawBorder(){
+        chocolate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable highlight = getResources().getDrawable(R.drawable.ic_border);
+                chocolate.setBackground(highlight);
+                cheese.setBackground(null);
+                matcha.setBackground(null);
+                strawberry.setBackground(null);
+                vanilla.setBackground(null);
+                selected = "chocolate";
+
+            }
+        });
+
+        cheese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable highlight = getResources().getDrawable( R.drawable.ic_border);
+                chocolate.setBackground(null);
+                cheese.setBackground(highlight);
+                matcha.setBackground(null);
+                strawberry.setBackground(null);
+                vanilla.setBackground(null);
+                selected = "cheese";
+
+            }
+        });
+
+        matcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable highlight = getResources().getDrawable( R.drawable.ic_border);
+                chocolate.setBackground(null);
+                cheese.setBackground(null);
+                matcha.setBackground(highlight);
+                strawberry.setBackground(null);
+                vanilla.setBackground(null);
+                selected = "matcha";
+            }
+        });
+
+        strawberry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable highlight = getResources().getDrawable( R.drawable.ic_border);
+                chocolate.setBackground(null);
+                cheese.setBackground(null);
+                matcha.setBackground(null);
+                strawberry.setBackground(highlight);
+                vanilla.setBackground(null);
+                selected = "strawberry";
+            }
+        });
+
+        vanilla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable highlight = getResources().getDrawable( R.drawable.ic_border);
+                chocolate.setBackground(null);
+                cheese.setBackground(null);
+                matcha.setBackground(null);
+                strawberry.setBackground(null);
+                vanilla.setBackground(highlight);
+                selected = "vanilla";
+            }
+        });
 
     }
 }
