8b6c746e1dc40dc2e4de40e240d01aa252842876
Vivihhw <huhu6404@uni.sydney.edu.au>
2019-10-22 15:25:05 +1100

    Add signin signup via Firbase

--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/SignInActivity.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/SignInActivity.java
@@ -3,40 +3,61 @@ package au.edu.sydney.comp5216.cakefactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.content.Intent;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Collections;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class SignInActivity extends AppCompatActivity {
     TextView signUp;
+    EditText email_edit;
+    EditText password_edit;
+    String email;
+    String password;
     ImageView signIn;
     private FirebaseFirestore mFirestore;
-    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private static final String TAG = SignInActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
         signUp = (TextView) findViewById(R.id.signUp);
+
         signIn = (ImageView) findViewById(R.id.signIn);
         FirebaseFirestore.setLoggingEnabled(true);
-        // Initialize Firestore and the main RecyclerView
         initFirestore();
-        startSignIn();
+        mAuth = FirebaseAuth.getInstance();
+        setUpListener();
 
+    }
 
+    /**
+     * Check if any edittext field is empty
+     * @return if edittext is empty, return true.
+     */
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return false;
+
+        return true;
+    }
+
+    private void setUpListener(){
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,8 +68,16 @@ public class SignInActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(SignInActivity.this, MainActivity.class);
-                startActivity(it);
+                email_edit = (EditText)findViewById(R.id.email);
+                password_edit = (EditText)findViewById(R.id.password);
+                if(isEmpty(email_edit)||isEmpty(password_edit)){
+                    Toast.makeText(SignInActivity.this, "Email or password can't be blank!", Toast.LENGTH_SHORT).show();
+                }else{
+                    email = email_edit.getText().toString();
+                    password = password_edit.getText().toString();
+                    startSignIn(email,password);
+                }
+
             }
         });
 
@@ -62,15 +91,29 @@ public class SignInActivity extends AppCompatActivity {
 //        return (!mViewModel.getIsSigningIn() && FirebaseAuth.getInstance().getCurrentUser() == null);
 //    }
 
-    private void startSignIn() {
-        // Sign in with FirebaseUI
-        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
-                .setAvailableProviders(Collections.singletonList(
-                        new AuthUI.IdpConfig.EmailBuilder().build()))
-                .setIsSmartLockEnabled(false)
-                .build();
+    private void startSignIn(String email, String password) {
 
-        startActivityForResult(intent, RC_SIGN_IN);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent it = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(it);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 //    @Override
--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/SignUpActivity.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/SignUpActivity.java
@@ -1,13 +1,105 @@
 package au.edu.sydney.comp5216.cakefactory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity  extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText email_edit;
+    EditText password_edit;
+    EditText mobile_edit;
+    EditText repassword_edit;
+    String email;
+    String password;
+    String mobile;
+    String repassword;
+    ImageView signIn;
+    private FirebaseAuth mAuth;
+    private static final String TAG = SignUpActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        signIn = (ImageView) findViewById(R.id.signIn);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore.setLoggingEnabled(true);
+        setUpListener();
+    }
 
+    /**
+     * Check if any edittext field is empty
+     *
+     * @return if edittext is empty, return true.
+     */
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return false;
+
+        return true;
+    }
+
+    private void setUpListener() {
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email_edit = (EditText) findViewById(R.id.email);
+                password_edit = (EditText) findViewById(R.id.password);
+                mobile_edit = (EditText) findViewById(R.id.mobile);
+                repassword_edit = (EditText) findViewById(R.id.repassword);
+                if (isEmpty(email_edit) || isEmpty(password_edit) || isEmpty(mobile_edit) || isEmpty(repassword_edit)) {
+                    Toast.makeText(SignUpActivity.this, "Email or password can't be blank!", Toast.LENGTH_SHORT).show();
+                } else {
+                    email = email_edit.getText().toString();
+                    password = password_edit.getText().toString();
+                    mobile = mobile_edit.getText().toString();
+                    repassword = repassword_edit.getText().toString();
+                    if(password.equals(repassword)){
+                        startSignIn(email, password);
+                    }else{
+                        Toast.makeText(SignUpActivity.this, "Password is not same! ", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+        });
+    }
+
+    private void startSignIn(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent it = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(it);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 }
