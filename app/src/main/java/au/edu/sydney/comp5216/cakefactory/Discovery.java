42b2173f34794e585b64f35d325837e9bc8f0b8f
Vivihhw <huhu6404@uni.sydney.edu.au>
2019-10-22 20:07:05 +1100

    Finish function search article

--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/Discovery.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/Discovery.java
@@ -1,36 +1,47 @@
 package au.edu.sydney.comp5216.cakefactory;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DataSnapshot;
+
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.Date;
+
 import com.bumptech.glide.Glide;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnCompleteListener;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 
 import adapter.ListBaseAdapter;
 import custom_font.ExpandableHeightListView;
 import model.Article;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
@@ -39,12 +50,13 @@ public class Discovery extends Fragment {
     private ExpandableHeightListView listview;
     private ArrayList<Article> Bean = new ArrayList<>();
     private ListBaseAdapter baseAdapter;
+    EditText search;
     private FirebaseFirestore db;
     private Query mQuery;
     DatabaseReference ref;
     CollectionReference articles;
     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    private static final String TAG =Discovery.class.getSimpleName();
+    private static final String TAG = Discovery.class.getSimpleName();
 
     private int[] IMAGE1 = {R.drawable.newsname1, R.drawable.newsname1, R.drawable.newsname1};
     private int[] IMAGE2 = {R.drawable.img1, R.drawable.img1, R.drawable.img1};
@@ -53,7 +65,7 @@ public class Discovery extends Fragment {
     private String[] NEWS = {"Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous. Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous",
             "Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous. Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous",
             "Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous. Trump’s Plan for AmericanMade iPhonew Wold Be Disastrous"};
-    private String[] NEWSSUB = {"Why even a President Trump couldn’t make Apple manufacture iPhone in the state.","Why even a President Trump couldn’t make Apple manufacture iPhone in the state.",
+    private String[] NEWSSUB = {"Why even a President Trump couldn’t make Apple manufacture iPhone in the state.", "Why even a President Trump couldn’t make Apple manufacture iPhone in the state.",
             "Why even a President Trump couldn’t make Apple manufacture iPhone in the state."};
 
     public Discovery() {
@@ -80,7 +92,7 @@ public class Discovery extends Fragment {
         return inflater.inflate(R.layout.discovery, container, false);
     }
 
-    private void readFromDatabase(){
+    private void readFromDatabase() {
         // Attach a listener to read the data at our posts reference
         db.collection("article")
                 .get()
@@ -97,7 +109,7 @@ public class Discovery extends Fragment {
                                 String image = document.get("image").toString();
                                 Date date = document.getDate("date");
                                 String strDate = dateFormat.format(date);
-                                addToList(author,title,content,strDate,sub,image);
+                                addToList(author, title, content, strDate, sub, image);
                                 Log.d(TAG, document.getId() + " => " + document.getData());
                             }
 
@@ -110,13 +122,15 @@ public class Discovery extends Fragment {
 
 
     }
-    private void addToList(String author, String title, String content, String date, String sub, String image){
-        Article article = new Article(IMAGE1[0],image,author,date,title,sub);
+
+    private void addToList(String author, String title, String content, String date, String sub, String image) {
+        Article article = new Article(IMAGE1[0], image, author, date, title, sub);
 
         Bean.add(article);
 
     }
-    private void addbaseAdapter(){
+
+    private void addbaseAdapter() {
         baseAdapter = new ListBaseAdapter(getContext(), Bean);
         listview.setAdapter(baseAdapter);
         baseAdapter.notifyDataSetChanged();
@@ -128,18 +142,11 @@ public class Discovery extends Fragment {
         FirebaseFirestore.setLoggingEnabled(true);
         initFirestore();
         readFromDatabase();
-//        Log.d(TAG, "Size is "+ Bean.size());
 
-       listview = getActivity().findViewById(R.id.listview);
-//
-//        for (int i= 0; i< IMAGE1.length; i++){
-//            Article bean = new Article(IMAGE1[i], IMAGE2[i],NEWSNAME[i], TITLE[i], NEWS[i], NEWSSUB[i]);
-//            Bean.add(bean);
-//        }
-//
-//        baseAdapter = new ListBaseAdapter(getContext(), Bean);
-//        listview.setAdapter(baseAdapter);
-//        baseAdapter.notifyDataSetChanged();
+
+        listview = getActivity().findViewById(R.id.listview);
+        search = (EditText) getActivity().findViewById(R.id.autotext);
+        addSearchListener();
 
 
 //
@@ -150,4 +157,47 @@ public class Discovery extends Fragment {
 //            }
 //        });
     }
+
+    /**
+     * Check if any edittext field is empty
+     *
+     * @return if edittext is empty, return true.
+     */
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return false;
+
+        return true;
+    }
+
+
+    public void addSearchListener() {
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEmpty(search)) {
+                    Toast.makeText(getContext(), "Please input what you want to search", Toast.LENGTH_SHORT).show();
+                } else {
+                    final ArrayList<Article> result = new ArrayList<>();
+                    String s = search.getText().toString();
+                    for (int i = 0; i < Bean.size(); i++) {
+                        Article a = Bean.get(i);
+                        String title = a.getNews();
+                        if (title.contains(s)) {
+                            result.add(a);
+                        }
+                    }
+
+                    baseAdapter = new ListBaseAdapter(getContext(), result);
+                    listview.setAdapter(baseAdapter);
+                    baseAdapter.notifyDataSetChanged();
+
+                }
+
+
+            }
+        });
+
+    }
+
 }
