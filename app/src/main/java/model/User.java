505c3e0497ff9cd76a770552286828a500848a19
sjin8759 <sjin8759@uni.sydney.edu.au>
2019-10-23 17:56:30 +1100

    Filter user's  article

---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,108 @@
+package model;
+
+import com.google.firebase.firestore.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+
+/**
+ * User POJO.
+ */
+@IgnoreExtraProperties
+public class User {
+
+    public static final String USERNAME = "username";
+    public static final String EMAIL = "email";
+    public static final String MOBILE = "mobile";
+    public static final String PASSWORD = "password";
+    public static final String AVATAR = "avatar";
+    public static final String ARTICLE = "articles";
+    public static final String DESIGN = "designs";
+    public static final String ORDER = "orders";
+
+    private String username;
+    private String email;
+    private String mobile;
+    private String password;
+    private String avatar;
+    private ArrayList<String> articles;
+    private ArrayList<String> designs;
+    private ArrayList<String> orders;
+
+    public User() {}
+
+    public User(String username, String email, String mobile, String password, String avatar,
+                ArrayList<String> articles, ArrayList<String> designs, ArrayList<String> orders) {
+        this.username = username;
+        this.email = email;
+        this.mobile = mobile;
+        this.password = password;
+        this.avatar = avatar;
+        this.articles = articles;
+        this.designs = designs;
+        this.orders = orders;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public ArrayList<String> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(ArrayList<String> articles) {
+        this.articles = articles;
+    }
+
+    public ArrayList<String> getDesigns() {
+        return designs;
+    }
+
+    public void setDesigns(ArrayList<String> designs) {
+        this.designs = designs;
+    }
+
+    public ArrayList<String> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<String> orders) {
+        this.orders = orders;
+    }
+}
