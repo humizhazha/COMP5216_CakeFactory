ae09832032c6fa846ff52815124e6686e198416f
chenzhiying <zche7547@uni.sydney.edu.au>
2019-10-24 22:32:03 +1100

    123

--- app/src/main/java/au/edu/sydney/comp5216/cakefactory/DesignDetail.java
+++ app/src/main/java/au/edu/sydney/comp5216/cakefactory/DesignDetail.java
@@ -2,6 +2,7 @@ package au.edu.sydney.comp5216.cakefactory;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,21 +11,45 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import model.DesignModel;
 
-public class DesignDetail extends AppCompatActivity {
+/**
+ * Design Detail Activity
+ * Show design'detail information
+ * Show selected shape, flavour, type and decoration design
+ */
+public class DesignDetail extends AppCompatActivity implements
+        View.OnClickListener,
+        EventListener<DocumentSnapshot> {
+
+    public static final String KEY_DESIGN_ID = "key_design_id";
+    private String designId;
 
     private DesignModel currentDesign;
     TextView flavour;
@@ -39,15 +64,28 @@ public class DesignDetail extends AppCompatActivity {
     private Drawable sprinkling;
     private ViewGroup cakeBase;
 
-    Button goCheckout;
+    private FirebaseFirestore db;
+    private DocumentReference mDesignRef;
+    private ListenerRegistration mDesignReg;
+    private CollectionReference design;
+
+    private Boolean isProfile = false;
+    private static final String TAG = DesignDetail.class.getSimpleName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.design_detail);
-        currentDesign = (DesignModel)getIntent().getSerializableExtra("design");
 
         //get drawable initilize with drawable
+        TextView toolbar = findViewById(R.id.toolbar);
+        toolbar.setText("Design summary");
+
+        Button save = findViewById(R.id.save);
+        ImageView submit = findViewById(R.id.submit);
+
+        //get drawable initialize with drawable
         strawberry = getResources().getDrawable(R.drawable.deco_strawberry);
         cookie = getResources().getDrawable(R.drawable.deco_cookie);
         marshmallow = getResources().getDrawable(R.drawable.deco_marshmallows);
@@ -55,31 +93,73 @@ public class DesignDetail extends AppCompatActivity {
         candle = getResources().getDrawable(R.drawable.deco_candle);
         sprinkling = getResources().getDrawable(R.drawable.deco_sprinkles);
         cakeBase = (ViewGroup) findViewById(R.id.base);
+        cakeBase = findViewById(R.id.base);
 
         //Initlize the textview
-        flavour = (TextView)findViewById(R.id.flavour_text);
+        flavour = (TextView) findViewById(R.id.flavour_text);
         type = (TextView) findViewById(R.id.type_text);
         shape = (TextView) findViewById(R.id.shape_text);
         date = (TextView) findViewById(R.id.date_text);
+        flavour = findViewById(R.id.flavour_text);
+        type = findViewById(R.id.type_text);
+        shape = findViewById(R.id.shape_text);
+        date = findViewById(R.id.date_text);
 
+        initFirestore();
+        findViewById(R.id.backArrow).setOnClickListener(this);
 
-        Button goCheckout = findViewById(R.id.goCheckOut);
-        goCheckout.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(DesignDetail.this, CheckOutActivity.class);
-                double price = calculatePrice(currentDesign.getDecorations());
-                // it.putExtra()
-                it.putExtra("price", price);
-                finish();
-                startActivity(it);
-            }
-        });
+        db = FirebaseFirestore.getInstance();
 
-        fillInInformation();
-        drawDecoration();
-
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey(KEY_DESIGN_ID)) {
+            isProfile = true;
+            save.setVisibility(View.GONE);
+            submit.setVisibility(View.VISIBLE);
+            designId = extras.getString(KEY_DESIGN_ID);
+            mDesignRef = db.collection("design").document(designId);
+            db.collection("design").document(designId).get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                currentDesign = document.toObject(DesignModel.class);
+                                shape.setText(currentDesign.getShape());
+                                flavour.setText(currentDesign.getFlavour());
+                                type.setText(currentDesign.getType());
+                                date.setText("24/10/2019");
+                                drawDecoration();
+                            } else {
+                                Log.d(TAG, "get failed with ", task.getException());
+                            }
+                        }
+                    });
+        } else {
+            currentDesign = (DesignModel) getIntent().getSerializableExtra("design");
+            fillInInformation();
+            drawDecoration();
+        }
     }
-    private void fillInInformation(){
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mDesignReg != null) {
+            mDesignReg = mDesignRef.addSnapshotListener(this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    private void initFirestore() {
+        db = FirebaseFirestore.getInstance();
+        design = db.collection("design");
+    }
+
+    private void fillInInformation() {
         //Set all the design information
         shape.setText(currentDesign.getShape());
         flavour.setText(currentDesign.getFlavour());
@@ -89,64 +169,123 @@ public class DesignDetail extends AppCompatActivity {
         date.setText(formatter.format(now));
 
     }
-    private void drawDecoration(){
-        ArrayList<String> decorations = new ArrayList<>();
-        ArrayList<Integer> X = new ArrayList<>();
-        ArrayList<Integer> Y = new ArrayList<>();
-        decorations = currentDesign.getDecorations();
-        X = currentDesign.getX();
-        Y = currentDesign.getY();
+
+    private void drawDecoration() {
+        ArrayList<String> decorations = currentDesign.getDecorations();
+        ArrayList<Integer> X = currentDesign.getX();
+        ArrayList<Integer> Y = currentDesign.getY();
         int w = (int) (50 * DesignDetail.this.getResources().getDisplayMetrics().density);
         int h = (int) (50 * DesignDetail.this.getResources().getDisplayMetrics().density);
-        for(int i=0;i<decorations.size()-1;i++){
+        for (int i = 0; i < decorations.size() - 1; i++) {
 
             ImageView temp = new ImageView(DesignDetail.this);
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(w, h);
             temp.setLayoutParams(layoutParams);
-            if(decorations.get(i).equals("strawberry")){
+            if (decorations.get(i).equals("strawberry")) {
                 temp.setImageDrawable(strawberry);
-            }else if(decorations.get(i).equals("sprinkling")){
+            } else if (decorations.get(i).equals("sprinkling")) {
                 temp.setImageDrawable(sprinkling);
-            }else if(decorations.get(i).equals("candle")){
+            } else if (decorations.get(i).equals("candle")) {
                 temp.setImageDrawable(candle);
-            }else if(decorations.get(i).equals("bean")){
+            } else if (decorations.get(i).equals("bean")) {
                 temp.setImageDrawable(beans);
-            }else if(decorations.get(i).equals("cookie")){
+            } else if (decorations.get(i).equals("cookie")) {
                 temp.setImageDrawable(cookie);
-            }else{
+            } else {
                 temp.setImageDrawable(marshmallow);
             }
-            temp.setX(X.get(i+1));
-            temp.setY(Y.get(i+1));
+            temp.setX(X.get(i + 1));
+            temp.setY(Y.get(i + 1));
             cakeBase.addView(temp);
 
         }
-
     }
 
 
-    private double calculatePrice(ArrayList<String> decorations){
-        double price = 88d;
-        for(int i=0;i<decorations.size()-1;i++){
+    public void submitDesign(View view) {
+        Intent it = new Intent(DesignDetail.this, CheckOutActivity.class);
+        double price = calculatePrice(currentDesign.getDecorations());
+        // it.putExtra()
+        it.putExtra("price", price);
+        finish();
+        startActivity(it);
+    }
 
-            if(decorations.get(i).equals("strawberry")){
-                price+=5;
-            }else if(decorations.get(i).equals("sprinkling")){
-                price+=5;
-            }else if(decorations.get(i).equals("candle")){
-                price+=5;
-            }else if(decorations.get(i).equals("bean")){
-                price+=5;
-            }else if(decorations.get(i).equals("cookie")){
-                price+=5;
-            }else{
-                price+=0;
+    public void saveDesign(View view) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("flavour", currentDesign.getFlavour());
+        data.put("shape", currentDesign.getShape());
+        data.put("type", currentDesign.getType());
+        data.put("X", currentDesign.getX());
+        data.put("Y", currentDesign.getY());
+        data.put("decorations", currentDesign.getDecorations());
+        design.document()
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                        Toast.makeText(DesignDetail.this, "Your design has been saved!", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+        Intent intent = new Intent(DesignDetail.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onEvent(DocumentSnapshot snapshot, FirebaseFirestoreException e) {
+        currentDesign = snapshot.toObject(DesignModel.class);
+
+        // Set all the design information
+        shape.setText(currentDesign.getShape());
+        flavour.setText(currentDesign.getFlavour());
+        type.setText(currentDesign.getType());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.backArrow) {
+            Intent intent = new Intent(DesignDetail.this, MainActivity.class);
+            if (isProfile) {
+                intent.putExtra("Profile", true);
+            } else {
+                intent.putExtra("Design", true);
+            }
+            overridePendingTransition(0, 0);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            finish();
+            startActivity(intent);
+        }
+    }
+
+    private double calculatePrice(ArrayList<String> decorations) {
+        double price = 88d;
+        for (int i = 0; i < decorations.size() - 1; i++) {
+
+            if (decorations.get(i).equals("strawberry")) {
+                price += 5;
+            } else if (decorations.get(i).equals("sprinkling")) {
+                price += 5;
+            } else if (decorations.get(i).equals("candle")) {
+                price += 5;
+            } else if (decorations.get(i).equals("bean")) {
+                price += 5;
+            } else if (decorations.get(i).equals("cookie")) {
+                price += 5;
+            } else {
+                price += 5;
             }
 
         }
         return price;
     }
-
-
-
 }
+
