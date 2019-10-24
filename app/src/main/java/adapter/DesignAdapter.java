ccc32ce6ee02d73e98937ac740c1f31dab751541
sjin8759 <sjin8759@uni.sydney.edu.au>
2019-10-24 16:29:00 +1100

    Add my design pages and jump to detailed designs

---
+++ app/src/main/java/adapter/DesignAdapter.java
@@ -0,0 +1,79 @@
+package adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+
+import au.edu.sydney.comp5216.cakefactory.R;
+import model.DesignModel;
+
+/**
+ * RecyclerView adapter for a list of Designs.
+ */
+public class DesignAdapter extends FirestoreAdapter<DesignAdapter.ViewHolder>{
+
+    public interface OnDesignSelectedListener {
+        void OnDesignSelectedListener(DocumentSnapshot design);
+    }
+
+    private OnDesignSelectedListener mListener;
+
+    public DesignAdapter(Query query, OnDesignSelectedListener listener) {
+        super(query);
+        mListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_my_design, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getSnapshot(position), mListener);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView shape;
+        TextView type;
+        TextView flavour;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            shape  = itemView.findViewById(R.id.shapeText);
+            type = itemView.findViewById(R.id.typeText);
+            flavour = itemView.findViewById(R.id.flavourText);
+        }
+
+        public void bind(final DocumentSnapshot snapshot,
+                         final OnDesignSelectedListener listener) {
+
+            final DesignModel design = snapshot.toObject(DesignModel.class);
+
+            shape.setText(design.getShape());
+            type.setText(design.getType());
+            flavour.setText(design.getFlavour());
+
+            // Click listener
+            itemView.findViewById(R.id.cardView).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        listener.OnDesignSelectedListener(snapshot);
+                    }
+                }
+            });
+        }
+    }
+}
